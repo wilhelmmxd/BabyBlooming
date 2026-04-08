1aafeaad9a788a4d44bd959a3330ffe20a2a2e2b
Bmorrissey34 <brendanmorrissey34@gmail.com>
2026-04-07 21:04:07 -0400

    fixed vercel deployments

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -1,3 +1,5 @@
+"use client"
+
 import { initializeApp, getApps } from "firebase/app"
 import { getAuth } from "firebase/auth"
 import { getFirestore } from "firebase/firestore"
@@ -9,6 +11,8 @@ const firebaseConfig = {
   storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID, 
+
 }
 
 const app = getApps().length ? getApps()[0] : initializeApp(firebaseConfig)
--- next.config.mjs
+++ next.config.mjs
@@ -3,15 +3,8 @@ const nextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
   images: {
     unoptimized: true,
-  },
-  staticPageGenerationTimeout: 0,
-  experimental: {
-    dynamicIO: true,
   },
 }
 
